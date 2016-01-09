70b7cc65f35abe1bcf71a0ba1eaa4ed53c7e1f51
Diego Henrique Domingues <diego.domingues16@gmail.com>
2016-01-09 11:32:47 -0200

    Organizing and improve User tests

--- spec/models/spree/user_spec.rb
+++ spec/models/spree/user_spec.rb
@@ -5,15 +5,57 @@ describe Spree::User do
     (1..length).map { (65 + rand(26)).chr }.join
   end
 
-  let(:user) { build(:user) }
+  subject(:user) { build(:user, phone: '(11) 2334-4433') }
 
-  it {
-    is_expected.to validate_presence_of(:cpf)
-    is_expected.to validate_presence_of(:first_name)
-    is_expected.to validate_presence_of(:last_name)
-    is_expected.to validate_presence_of(:date_of_birth)
-    is_expected.to validate_presence_of(:phone)
-  }
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:cpf)}
+    it { is_expected.to validate_presence_of(:first_name)}
+    it { is_expected.to validate_presence_of(:last_name)}
+    it { is_expected.to validate_presence_of(:date_of_birth)}
+    it { is_expected.to validate_presence_of(:phone)}
+
+    context 'cpf' do
+      it 'returns valid' do
+        user.cpf = CPF.generate(true)
+
+        expect(user).to be_valid
+      end
+
+      context 'when the length is incorrect' do
+        it 'returns invalid' do
+          user.cpf = '123456789'
+
+          expect(user).to_not be_valid
+        end
+      end
+
+      context 'when the number is incorrect' do
+        it 'returns invalid' do
+          user.cpf = '12345678910'
+
+          expect(user).to_not be_valid
+        end
+      end
+    end
+
+    it 'validates length of first_name' do
+      user.first_name = string_with_length(101)
+
+      expect(user).to_not be_valid
+    end
+
+    it 'validates length of last_name' do
+      user.last_name = string_with_length(101)
+
+      expect(user).to_not be_valid
+    end
+
+    it 'validates date_of_birth is greater or equal to 18 years' do
+      user.date_of_birth = 17.years.ago
+
+      expect(user).to_not be_valid
+    end
+  end
 
   context '#full_name' do
     it 'should return first_name + last_name' do
@@ -31,58 +73,69 @@ describe Spree::User do
     end
   end
 
-  it 'should respond to extra properties' do
+  it 'responds to extra properties' do
     expect(user).to respond_to :first_name, :last_name, :cpf, :date_of_birth, :phone, :alternative_phone
   end
 
-  it 'should save user with success' do
+  it 'saves user with success' do
     expect(user.save!).to be_truthy
   end
 
-  it 'should save user with sanitize cpf' do
-    cpf_formatted = CPF.generate(true)
-    user.cpf = cpf_formatted
-    cpf = CPF.new(cpf_formatted)
+  context '#phone_area_code' do
+    context 'when there is phone' do
+      before { subject.valid? }
 
-    user.save!
+      it 'returns only the phone area code' do
+        expect(subject.phone_area_code).to eq('11')
+      end
+    end
 
-    expect(user.cpf).to eq(cpf.stripped)
+    context 'when there is not phone' do
+      before { subject.phone = nil }
+
+      it 'returns nil' do
+        expect(subject.phone_area_code).to be_nil
+      end
+    end
   end
 
-  it 'should save user with sanitize phone' do
-    user.phone = '(011) 3882-8877'
-    user.save!
+  context '#phone_number' do
+    context 'when there is phone' do
+      before { subject.valid? }
 
-    expect(user.phone).to eq('01138828877')
+      it 'returns only the phone number' do
+        expect(subject.phone_number).to eq('23344433')
+      end
+    end
+
+    context 'when there is not phone' do
+      before { subject.phone = nil }
+
+      it 'returns nil' do
+        expect(subject.phone_number).to be_nil
+      end
+    end
   end
 
-  it 'should validate cpf' do
-    expect(user).to be_valid
+  context 'sanitize' do
+    context 'phone' do
+      it 'keeps only numbers' do
+        subject.valid?
 
-    user.cpf = '123456789'
+        expect(subject.phone).to eq('1123344433')
+      end
+    end
 
-    expect(user).to_not be_valid
+    context 'cpf' do
+      it 'keeps only number' do
+        cpf_formatted = CPF.generate(true)
+        user.cpf = cpf_formatted
+        cpf = CPF.new(cpf_formatted)
 
-    user.cpf = '12345678910'
+        user.valid?
 
-    expect(user).to_not be_valid
-  end
-
-  it 'should validate length of first_name' do
-    user.first_name = string_with_length(101)
-
-    expect(user).to_not be_valid
-  end
-
-  it 'should validate length of last_name' do
-    user.last_name = string_with_length(101)
-
-    expect(user).to_not be_valid
-  end
-
-  it 'should validate date_of_birth is greater or equal to 18 years' do
-    user.date_of_birth = 17.years.ago
-
-    expect(user).to_not be_valid
+        expect(user.cpf).to eq(cpf.stripped)
+      end
+    end
   end
 end
