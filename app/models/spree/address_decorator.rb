43f90804ff8cca2824f3a0054b4f89cf19f09780
Diego Henrique Domingues <diego.domingues16@gmail.com>
2016-01-09 11:32:08 -0200

    Adding phone_area_code and phone_number on Address an improve tests

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -3,4 +3,19 @@ Spree::Address.class_eval do
 
   validates :number, :numericality => {greater_than: 0}, :presence => true
   validates :district, length: { maximum: 150}, presence: true
+
+  before_validation :sanitize_phone
+
+  def phone_area_code
+    phone[0..1] if phone
+  end
+
+  def phone_number
+    phone[2..-1] if phone
+  end
+
+  private
+  def sanitize_phone
+    phone.gsub!(/[^\d]/, '')
+  end
 end
--- spec/models/spree/address_spec.rb
+++ spec/models/spree/address_spec.rb
@@ -1,29 +1,75 @@
 require 'spec_helper'
 
 describe Spree::Address do
-  let(:address) {build(:address)}
+  subject { build(:address, phone: '(11) 2334-4433') }
 
-  it { is_expected.to validate_presence_of(:number) }
-  it { is_expected.to validate_presence_of(:district) }
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:number) }
+    it { is_expected.to validate_presence_of(:district) }
 
-  it 'should validate length of district' do
-    address.district = string_with_length(151)
+    context 'phone' do
+      it 'validates length of district' do
+        subject.district = string_with_length(151)
 
-    expect(address).to_not be_valid
-  end
+        expect(subject).to_not be_valid
+      end
 
-  it 'should validate number negative' do
-    address.number = -1
+      it 'validates number negative' do
+        subject.number = -1
 
-    expect(address).to_not be_valid
+        expect(subject).to_not be_valid
+      end
+    end
   end
 
   context '#save' do
-    subject { address }
-
-    before { address.save! }
+    before { subject.save! }
 
     it { is_expected.to be_persisted }
+  end
+
+  context '#phone_area_code' do
+    context 'when there is phone' do
+      before { subject.valid? }
+
+      it 'returns only the phone area code' do
+        expect(subject.phone_area_code).to eq('11')
+      end
+    end
+
+    context 'when there is not phone' do
+      before { subject.phone = nil }
+
+      it 'returns nil' do
+        expect(subject.phone_area_code).to be_nil
+      end
+    end
+  end
+
+  context '#phone_number' do
+    context 'when there is phone' do
+      before { subject.valid? }
+
+      it 'returns only the phone number' do
+        expect(subject.phone_number).to eq('23344433')
+      end
+    end
+
+    context 'when there is not phone' do
+      before { subject.phone = nil }
+
+      it 'returns nil' do
+        expect(subject.phone_number).to be_nil
+      end
+    end
+  end
+
+  context 'sanitize_phone' do
+    it 'keeps only numbers' do
+      subject.valid?
+
+      expect(subject.phone).to eq('1123344433')
+    end
   end
 
   def string_with_length(length)
